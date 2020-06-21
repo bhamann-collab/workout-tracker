717d2e9c67bb22a1abdbbbfce15ae52dcaf40faa
bhamann-collab <brockohamann@gmail.com>
2020-06-22 00:06:39 +0930

    Added routes

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
+const { stringify } = require('querystring')
 const app = express()
 
 const PORT = process.env.PORT || 5001
 
 //Middleware
 app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(express.json())
 
-//Routes
+// Mongoose --------
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/workout', {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+mongoose.Promise = global.Promise
+
+//Schema
+let Schema = mongoose.Schema
+
+let ExerciseSchema = new Schema({
+    type: String,
+    name: String,
+    weight: Number,
+    sets: Number,
+    reps: Number,
+    duration: Number
+})
+
+let Exercise = mongoose.model('exercise', ExerciseSchema)
+//Schema
+// Mongoose --------
+
+//HTML Routes --------------------------
 app.get('/', (req, res) => {
     res.sendFile('/index.html', { root: __dirname })
 })
@@ -15,6 +40,21 @@ app.get('/', (req, res) => {
 app.get('/exercise', (req, res) => {
     res.sendFile('/public/exercise.html', { root: __dirname })
 })
+//HTML Routes --------------------------
 
+//API Routes --------------------------
+app.post('/api/workouts', (req, res) => {
+    console.log("test")
+})
+
+app.put('/api/workouts/:params', (req, res) => {
+    console.log(req.body)
+    
+    let exercise = new Exercise(req.body)
+    exercise.save()
+
+    //res.redirect()
+})
+//API Routes --------------------------
 
 app.listen(PORT, () => console.log(`Example app listening at http:localhost:${PORT}`))
